40177ad8e48e31954f66fe3ab0b73d8f8103539a
Aleks-TO <114433644+Aleks-TO@users.noreply.github.com>
2023-06-30 09:02:54 +0300

    fix useeffect modal

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -11,7 +11,7 @@ export const Modal = ({ onClose, url }) => {
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
-  }, []);
+  }, [onClose]);
 
   function handleKeyDown(event) {
     if (event.code === 'Escape') {
